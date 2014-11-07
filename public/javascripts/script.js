e31cdf50ae7d347c2957e30c4a92481ba71fd438
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2014-11-07 16:33:14 -0200

    Initial version

---
+++ public/javascripts/script.js
@@ -0,0 +1,44 @@
+$(function() {
+  function add_tweet(t) {
+    var li = $(
+      '<li>'
+     +  '<div class="image">'
+     +      '<img src="' + t.profile_image_url + '">'
+     +  '</div>'
+     +  '<div class="tweet">'
+     +      '<div class="screen_name">' + t.screen_name + '</div>'
+     +      '<div class="name"><a href="https://twitter.com/' + t.name + '">@' + t.name + '</a></div>'
+     +      '<div class="created_at">' + t.created_at + '</div>'
+     +  '<div class="text">' + t.text + '</div>'
+     +'</li>'
+    );
+    $('ul').prepend(li);
+  };
+
+  //Referencia: http://www.html5rocks.com/pt/tutorials/websockets/basics/
+  var ws = new WebSocket('ws://' + location.hostname + ':8000');
+
+  ws.onopen = function () {
+    console.log('Conectado no servidor');
+  };
+
+  ws.onerror = function (error) {
+    console.error(error);
+  };
+
+  ws.onmessage = function (e) {
+    console.log('Message:', e.data);
+    var t = JSON.parse(e.data);
+    add_tweet(t);
+  };
+
+  $('#last-update').text(new Date().toTimeString()); 
+
+  /*add_tweet({
+    name: 'fsilva',
+    screen_name: 'Fulano Silva',
+    profile_image_url: 'http://pbs.twimg.com/profile_images/2934829341/ef41c867b555ababcdda0200513b2211_normal.jpeg',
+    created_at: 'Fri Nov 07 14:06:46 +0000 2014',
+    text: 'Algumas campanhas irão escalar suas estruturas na @GetupCloud neste #BlackFriday - você está preparado? #paas #escalabilidade #openshift'
+  });*/
+})
--- server.js
+++ server.js
@@ -1,159 +1,112 @@
-#!/bin/env node
-//  OpenShift sample Node application
+#!/usr/bin/env node
+
+var addr   = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1';
+var port   = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8000;
+
 var express = require('express');
 var fs      = require('fs');
+var twitter = require('twitter');
+var ws      = require('ws');
+var path    = require('path');
+var http    = require('http');
 
+// Fill with your Twitter credentials //
+var TWITTER_API_KEY             = process.env.TWITTER_API_KEY;
+var TWITTER_API_SECRET          = process.env.TWITTER_API_SECRET;
+var TWITTER_ACCESS_TOKEN        = process.env.TWITTER_ACCESS_TOKEN;
+var TWITTER_ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET;
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+if (!(TWITTER_API_KEY && TWITTER_API_SECRET && TWITTER_ACCESS_TOKEN && TWITTER_ACCESS_TOKEN_SECRET)) {
+  console.error('Missing Twitter key and access token.\n');
+  console.error(' TWITTER_API_KEY\n TWITTER_API_SECRET\n TWITTER_ACCESS_TOKEN\n TWITTER_ACCESS_TOKEN_SECRET')
+  require('sleep').sleep(1);
+  process.exit(1);
+}
 
-    //  Scope.
-    var self = this;
+// Term used to filter on twitter //
+var searchTerm = process.env.HASHTAG || [ '#getupcloud', '@getupcloud' ];
 
+// Web Server //
+var app = express();
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware({ src: __dirname + '/public', force: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server = http.createServer(app);
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
+// WebSocket //
+var wss = new ws.Server({server: server});
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+wss.on('connection', function(ws) {
+  console.log('New WS client');
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+  ws.on('close', function(code, message) {
+    console.log('Cliente desconectado');
+  });
+});
 
+wss.broadcast = function(data) {
+  for(var i in this.clients) {
+    this.clients[i].send(JSON.stringify(data));
+  }
+};
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
+// Twitter Stream //
+if (process.env.DEBUG != 'true') { // try not to reach twitter connection limits while in dev
 
+var t = new twitter({
+    consumer_key:        TWITTER_API_KEY,
+    consumer_secret:     TWITTER_API_SECRET,
+    access_token_key:    TWITTER_ACCESS_TOKEN,
+    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+});
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
+t.stream('statuses/filter', { track: searchTerm }, function(stream) {
+  stream.on('error', function(error) {
+    console.error('ERROR:', error);
+  });
 
+  stream.on('data', function(tweet) {
+    wss.broadcast({
+      name:              tweet.user.name,
+      screen_name:       tweet.user.screen_name,
+      profile_image_url: tweet.user.profile_image_url,
+      created_at:        tweet.created_at,
+      text:              tweet.text
+    });
+  });
+});
 
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
+}
 
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
+// Signal handlers (optional) //
+process.on('exit', function() {
+  // give a chance to do something upon exit
+  console.log('bye...');
+});
 
+[ 'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+  'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM' ].forEach(function(sig, index, array) {
+  process.on(sig, function() {
+    if (typeof sig === "string") {
+      console.log('%s: Received %s - terminating sample app ...', Date(Date.now()), sig);
+      console.log('%s: Node server stopped.', Date(Date.now()) );
+      process.exit(1);
+    }
+  });
+});
 
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+// Start the app //
+server.listen(port, addr);
+console.log("App express escutando %s:%s", addr, port);
 
